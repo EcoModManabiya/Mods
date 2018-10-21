10b371c0875f4991d60c5f72b27da712bfd8ab47
UresiiZo <fi.zero.1972@gmail.com>
2018-10-21 16:50:50 +0900

    Update BATfile

--- ManabiYaRecipes/manabiya.bat
+++ ManabiYaRecipes/manabiya.bat
@@ -11,9 +11,11 @@ if %result%==true (
 	echo Override the file.
 	ren ..\Objects\ComputerLabObject.cs ComputerLabObject.csbak	
 	ren ..\Tools\AxeItem.cs AxeItem.csbak	
+	ren ..\AutoGen\Item\Board.cs Board.csbak	
 ) else if %result%==false (
 	echo Restore to original files. And You must delete ManabiYa Mod.
 	ren ..\Objects\*.csbak *.cs
 	ren ..\Tools\*.csbak *.cs
+	ren ..\AutoGen\Item\*.csbak *.cs	
 )
 @pause
