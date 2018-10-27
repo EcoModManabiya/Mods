f3886f882f061b353b6b4118202dfcbebb64eb2e
UresiiZo <fi.zero.1972@gmail.com>
2018-10-27 21:44:36 +0900

    Update Barrel

--- ManabiYaPlus/Items/MYUsedBarrel.cs
+++ ManabiYaPlus/Items/MYUsedBarrel.cs
@@ -5,37 +5,61 @@ namespace Eco.Mods.TechTree
     using System.ComponentModel;
     using Eco.Gameplay.Blocks;
     using Eco.Gameplay.Components;
+    using Eco.Gameplay.Components.Auth;
     using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Economy;
+    using Eco.Gameplay.Housing;
+    using Eco.Gameplay.Interactions;
     using Eco.Gameplay.Items;
+    using Eco.Gameplay.Minimap;
     using Eco.Gameplay.Objects;
     using Eco.Gameplay.Players;
+    using Eco.Gameplay.Property;
     using Eco.Gameplay.Skills;
     using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Gameplay.Pipes.LiquidComponents;
+    using Eco.Gameplay.Pipes.Gases;
+    using Eco.Gameplay.Systems.Tooltip;
+    using Eco.Shared;
+    using Eco.Shared.Math;
     using Eco.Shared.Localization;
     using Eco.Shared.Serialization;
     using Eco.Shared.Utils;
-    using Eco.World;
-    using Eco.World.Blocks;
+    using Eco.Shared.View;
+    using Eco.Shared.Items;
     using Eco.Gameplay.Pipes;
-    using Eco.Gameplay.Pipes.LiquidComponents; 
+    using Eco.World.Blocks;
 
 
     [Serialized]
-    [Solid]
-    [RequiresSkill(typeof(PetrolRefiningEfficiencySkill), 0)]   
-    public partial class MYUsedBarrelBlock :
-        PickupableBlock      
-        , IRepresentsItem     
+    [RequireComponent(typeof (PropertyAuthComponent))]
+    [RequireComponent(typeof (SolidGroundComponent))]            
+    public partial class MYUsedBarrelObject :
+        WorldObject,    
+        IRepresentsItem
     {
-        public Type RepresentedItemType { get { return typeof(MYUsedBarrelItem); } }    
-    }
+        public override string FriendlyName { get { return "Used Barrel"; } } 
 
+        public virtual Type RepresentedItemType { get { return typeof(MYUsedBarrelItem); } } 
+
+
+        protected override void Initialize()
+        {
+
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+        }
+    }
+	
     [Serialized]
     [MaxStackSize(10)]                                       
     [Weight(2000)]      
     [Currency]              
     public partial class MYUsedBarrelItem :
-    BlockItem<MYUsedBarrelBlock>
+    WorldObjectItem<MYUsedBarrelObject>
     {
         public override string FriendlyName { get { return "Used Barrel"; } } 
         public override string Description { get { return "To reuse it you have to wash it."; } }
